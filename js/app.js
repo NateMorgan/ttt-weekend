93cbf567d669986e2ff72386df8e614315f65020
Nate Morgan <nathanielrmorgan@gmail.com>
2022-09-01 20:10:16 -0400

    picking squares and getting index working

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const playerOne = "X"
+const playerTwo = "O"
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let board = [null,null,null,null,null,null,null,null,null]
+let player = 1
+let gameover = false
 
 /*------------------------ Cached Element References ------------------------*/
+const boardEle = document.getElementById('game-space')
 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+boardEle.addEventListener('click',pickSquare)
 
 
 /*-------------------------------- Functions --------------------------------*/
+function pickSquare(evt){
+  targetSquare = evt.target
 
+  targetSquareIter = parseInt(targetSquare.id.at(-1))
+
+
+}
